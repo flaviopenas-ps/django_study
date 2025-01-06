510b2468abdd9bfcda51773b515e832cbee0cd75
flavio penas <flaviopenas@gmail.com>
2025-01-06 17:32:09 +0000

    new model added: Notes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,9 @@
 
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import Note
 
 
 # generics.CreateAPIView is to see API website type page
@@ -10,3 +11,27 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class CreateNoteList(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+
+class DeleteNote(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
